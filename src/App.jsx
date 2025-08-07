92ad92b59ae6b880e265eda2e72db187d608e0ec
basudevkumer <jhulonkumar40@gmail.com>
2025-08-07 14:37:14 +0600

    test

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import Menu from "./component/navbar/Menu";
+
+function App() {
+  return <Menu />;
+}
+
+export default App;
---
+++ src/component/navbar/Menu.jsx
@@ -0,0 +1,7 @@
+import "./menu.css";
+
+function Menu() {
+  return <h1>hello world</h1>;
+}
+
+export default Menu;
